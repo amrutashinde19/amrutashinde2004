028a1be327acf5a39ec52c02e35c08f3879b61a2
amrutashinde19 <b51_2022_ecse@yes.edu.in>
2025-06-06 21:12:46 +0530

    student.py

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from catboost import catBoostClassifier
+from catboost import catboostClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
