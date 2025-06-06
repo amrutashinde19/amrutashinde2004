cef18cade9d9e080597fe5d4ba2bda4439952b50
amrutashinde19 <b51_2022_ecse@yes.edu.in>
2025-06-06 20:26:07 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from catboost import CatBoostClassifier
+from xgboost import XGBClassifier
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sns
